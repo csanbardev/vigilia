18c3d7a62e5e51bba0bb62f1392f6b9cdd9fc4b3
csanbardev <csanbardev@gmail.com>
2024-11-25 09:08:19 +0100

    implement event in Staff to start TV puzzle

--- Scripts/Objects/bath_door.gd
+++ Scripts/Objects/bath_door.gd
@@ -2,10 +2,11 @@ extends StaticBody2D
 
 var player_on_area:bool = false
 var is_open:bool = false
+@export var event_controller: Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	event_controller.connect("custom_event", Callable(self, "_close_door"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +14,14 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and player_on_area and not is_open:
 		$BathDoorAnimation.play("Open")
 		$BathDoorCollision.disabled = true
+		$BathDoorArea/CollisionShape2D.disabled = false
 		is_open = true
+
+func _close_door(event_name: String) -> void:
+	if event_name == "tv":
+		$BathDoorAnimation.play("Close")
+		await get_tree().create_timer(0.01).timeout
+		$BathDoorCollision.disabled = false
 
 func _on_bath_door_area_body_entered(body: Node2D) -> void:
 	player_on_area = true
--- Scripts/Objects/tv.gd
+++ Scripts/Objects/tv.gd
@@ -2,18 +2,26 @@ extends StaticBody2D
 
 var tv_dialogue
 var item_added:bool = false
+@export var event_controller: Area2D
 
 func _ready():
 	tv_dialogue = $TVDialogue
 	tv_dialogue.connect("dialogue_start_activate", Callable(self, "_on_tv_start_activate"))
+	event_controller.connect("custom_event", Callable(self, "start_tv_puzzle"))
 
 func _process(delta: float) -> void:
 	# check if tv was broken and finish puzzle
 	if States.break_tv and !item_added:
 		finish_tv_puzzle()
 
-func _on_tv_start_activate():
-	$TVDialogue.dialogue_start = "tv_new_start"
+func _on_tv_start_activate(event_name: String):
+	if event_name == "tv":
+		$TVDialogue.dialogue_start = "tv_new_start"
+	
+
+func start_tv_puzzle(event_name: String) -> void:
+	if event_name == "tv":
+		$TVAnimation.play("On")
 
 func finish_tv_puzzle() -> void:
 	var collectedItems = get_tree().current_scene.get_node("Player").get_node("CollectedItems")
