cd1d91ae4a0452cacdeaba5c3e21d3ebc04beb6b
csanbardev <csanbardev@gmail.com>
2024-11-25 12:45:56 +0100

    fix player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -39,17 +39,41 @@ func _physics_process(delta: float) -> void:
 	# Actualizar la velocidad en base al vector de entrada y la velocidad configurada
 	velocity = input_vector * SPEED
 	
-	if Input.is_action_just_pressed("ui_right"):
-		$PlayerAnimation.play("Run_Right")
+	# Cambiar animaciones basadas en la entrada
+	if input_vector.length() > 0:  # Si hay movimiento
+		if Input.is_action_just_pressed("ui_right"):
+			$PlayerAnimation.play("Run_Right")
+		elif Input.is_action_just_pressed("ui_left"):
+			$PlayerAnimation.play("Run_Left")
+		elif Input.is_action_just_pressed("ui_down"):
+			$PlayerAnimation.play("Run_Down")
+		elif Input.is_action_just_pressed("ui_up"):
+			$PlayerAnimation.play("Run_Up")
+	else:  # Si no hay movimiento, se usa la animación Idle
+		# Obtener la dirección del ratón con respecto al jugador
+		var mouse_position = get_global_mouse_position()
+		var direction_to_mouse = (mouse_position - global_position).normalized()
 		
-	if Input.is_action_just_pressed("ui_left"):
-		$PlayerAnimation.play("Run_Left")
-		
-	if Input.is_action_just_pressed("ui_down"):
-		$PlayerAnimation.play("Run_Down")
-		
-	if Input.is_action_just_pressed("ui_up"):
-		$PlayerAnimation.play("Run_Up")
+		# Obtener el ángulo entre la dirección hacia el ratón y la dirección del jugador
+		var angle = direction_to_mouse.angle()
+
+		# Determinar la dirección basándonos en el ángulo
+		if angle >= -PI / 4 and angle < PI / 4:
+			# Mirando a la derecha
+			$PlayerAnimation.play("Idle")
+			$PlayerAnimation.frame = 0
+		elif angle >= PI / 4 and angle < 3 * PI / 4:
+			# Mirando arriba
+			$PlayerAnimation.play("Idle")
+			$PlayerAnimation.frame = 3
+		elif angle >= -3 * PI / 4 and angle < -PI / 4:
+			# Mirando abajo
+			$PlayerAnimation.play("Idle")
+			$PlayerAnimation.frame = 1
+		else:
+			# Mirando a la izquierda
+			$PlayerAnimation.play("Idle")
+			$PlayerAnimation.frame = 2
 
 	# Mover al personaje
 	move_and_slide()
