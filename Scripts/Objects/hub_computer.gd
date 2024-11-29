d4fc4f22f04d98ac7f4fdc0fbc109461d41c9cc4
csanbardev <csanbardev@gmail.com>
2024-11-29 18:42:12 +0100

    create hub computer scene

---
+++ Scripts/Objects/hub_computer.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+var is_player_in_area: bool = false
+@export var numbers_panel: PackedScene
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and is_player_in_area:
+		$NumbersPanel.visible = true
+		get_tree().paused = true
+		
+
+func _on_hub_computer_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_player_in_area = true
+
+func _on_hub_computer_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_player_in_area = false
