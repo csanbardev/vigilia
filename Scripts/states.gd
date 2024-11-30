3cf75e0a0681a4e7a625f3dcc0f9f124cceabac1
csanbardev <csanbardev@gmail.com>
2024-11-30 13:49:00 +0100

    implement dynamic character name in dialogues

--- Scripts/states.gd
+++ Scripts/states.gd
@@ -3,5 +3,5 @@ extends Node
 var has_tv_object: bool = false
 var break_tv: bool = false
 var player_name: String = "Artorias"
-
+var character_name: String = "Vigi"
 var exit_code : Array = [2, 4, 5, 8]
