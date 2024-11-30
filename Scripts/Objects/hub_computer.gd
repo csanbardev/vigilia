b7b8158aa60b4dfa82b7665a83f962fa40daea51
csanbardev <csanbardev@gmail.com>
2024-11-30 11:01:06 +0100

    stop/pause hub computer animation

--- Scripts/Objects/hub_computer.gd
+++ Scripts/Objects/hub_computer.gd
@@ -4,9 +4,12 @@ var is_player_in_area: bool = false
 @export var numbers_panel: PackedScene
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and is_player_in_area:
+		$AnimatedSprite2D.play()
 		$NumbersPanel.visible = true
 		get_tree().paused = true
 		
+	if $NumbersPanel.visible == false:
+		$AnimatedSprite2D.stop()
 
 func _on_hub_computer_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
