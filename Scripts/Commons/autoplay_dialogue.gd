b4886e21546b9c97770bacd41934bf2d7f1d5c3e
csanbardev <csanbardev@gmail.com>
2024-12-02 09:31:02 +0100

    create autoplay dialogue

---
+++ Scripts/Commons/autoplay_dialogue.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var is_player_on_area:bool = false
+var tip_played:bool = false
+var dialogue_start_flag:bool = false
+signal dialogue_start_activate
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+@export var auto_remove: bool = false
+
+func _ready() -> void:
+	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_closed"))
+
+
+func init_dialogue () -> void:
+	if !dialogue_start_flag:
+		get_tree().get_first_node_in_group("Player").set_player_paused(true)
+		
+		DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+		if !dialogue_start_flag:
+			dialogue_start_flag = true
+			emit_signal("dialogue_start_activate")
+		
+		if auto_remove:
+			queue_free()
+
+func _on_dialogue_closed(resource: DialogueResource) -> void:
+	dialogue_start_flag = false
+	# let player move
+	get_tree().get_first_node_in_group("Player").set_player_paused(false)
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_player_on_area = true
+		init_dialogue()
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		is_player_on_area = false
