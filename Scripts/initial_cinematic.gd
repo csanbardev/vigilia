3ebf4d6e9e7f230aba2a66555963394eb2d5c59f
csanbardev <csanbardev@gmail.com>
2024-12-03 18:21:53 +0100

    create initial cinematic

---
+++ Scripts/initial_cinematic.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var dialogue_resource: DialogueResource
+var dialogue_start: String = "initial_cinematic"
+
+func _ready() -> void:
+	DialogueManager.connect("dialogue_ended", Callable(self, "_on_dialogue_closed"))
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+
+func _on_dialogue_closed(resource: DialogueResource) -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_01.tscn")
--- Scripts/select_name.gd
+++ Scripts/select_name.gd
@@ -5,4 +5,4 @@ extends Control
 func _on_name_text_submitted(new_text: String) -> void:
 	if new_text != null:
 		States.player_name = new_text
-	get_tree().change_scene_to_file("res://Scenes/level_01.tscn")
+	get_tree().change_scene_to_file("res://Scenes/initial_cinematic.tscn")
