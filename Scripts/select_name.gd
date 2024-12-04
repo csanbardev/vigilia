76c08029c2f7fec9d8863099773a5d4f30276866
csanbardev <csanbardev@gmail.com>
2024-12-04 13:44:52 +0100

    add new button to select name

--- Scripts/select_name.gd
+++ Scripts/select_name.gd
@@ -3,6 +3,10 @@ extends Control
 
 
 func _on_name_text_submitted(new_text: String) -> void:
-	if new_text != null:
+	if new_text != "":
 		States.player_name = new_text
 	get_tree().change_scene_to_file("res://Scenes/initial_cinematic.tscn")
+
+
+func _on_acept_pressed() -> void:
+	_on_name_text_submitted($Name.text)
